f60e5f20130e43e6ae89ab2e5edc99698b760618
Cristian Ronquillo <cristian.ronquillol@ug.edu.ec>
2021-09-04 14:33:29 -0500

    week 4

---
+++ module4_solution/script.js
@@ -0,0 +1,17 @@
+(function () {
+
+    var names = ["Peter", "May", "Job", "Javier", "Paul", "Mark", "Harry", "Tony", "Ben", "Jimmy"];
+    
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    
+      if (firstLetter === 'j') {
+        byeSpeaker(names[i]);
+      }
+      else {
+        helloSpeaker(names[i]);
+      }
+    }
+    
+    })();
+    
